0921c9d0ee0611d0f6c2eeeb4bc0af66493559ec
zdenekd <ditezdenek@gmail.com>
2019-12-02 00:14:58 +0100

    Make some small fixes

--- src/components/content/content.jsx
+++ src/components/content/content.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import styles from './content.css';
 
-const Content = () => <article className={styles.default}>Content</article>;
+const Content = () => <article>Content</article>;
 
 export default Content;
--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import styles from './footer.css';
 
 const Footer = () => (
-    <footer className={styles.default}>
+    <footer>
         &copy; Fit s Marky | All Rights Reserved
         Telefon: <a href="tel:728618337" title="Telefon">728 618 337</a>
         E-mail: <a href="mailto:fitsmarky@seznam.cz" title="E-mail: fitsmarky@seznam.cz">fitsmarky@seznam.cz</a>
