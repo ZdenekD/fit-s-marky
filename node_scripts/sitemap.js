4ac0d8154b306681a8453cb180b9534a50336a47
zdenekd <ditezdenek@gmail.com>
2020-01-17 15:53:31 +0100

    Install sitemap, Create script for generate sitemap

---
+++ node_scripts/sitemap.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const {SitemapStream, streamToPromise} = require('sitemap');
+const config = require('../config.json');
+const {pages} = require('../src/data/pages.json');
+
+const sitemap = new SitemapStream({
+    hostname: config.production,
+});
+
+sitemap.write({
+    url: '/',
+    changefreq: 'yearly',
+    priority: 1,
+});
+
+pages.forEach(page => sitemap.write({
+    url: `/${page.slug}`,
+    changefreq: 'yearly',
+    priority: 0.4,
+}));
+
+sitemap.end();
+
+streamToPromise(sitemap)
+    .then(item => fs.writeFileSync(`${config.output.dir}/sitemap.xml`, item.toString()))
+    .catch(error => console.log(error));
