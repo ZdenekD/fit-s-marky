d8643ecde5ccd0f53309980b81a5894488e787c2
zdenekd <ditezdenek@gmail.com>
2020-03-27 23:39:14 +0100

    Update webpack config file (remove unnecessary strings)

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -119,7 +119,7 @@ module.exports = () => ({
             {
                 test: /\.js(x)?$/,
                 include: path.resolve(__dirname, 'src'),
-                exclude: /node_modules|bower_components|vendor/,
+                exclude: /node_modules|vendor/,
                 use: [
                     {
                         loader: 'babel-loader?cacheDirectory',
@@ -131,7 +131,7 @@ module.exports = () => ({
             {
                 test: /\.css$/,
                 include: path.resolve(__dirname, 'src'),
-                exclude: /node_modules|bower_components|vendor/,
+                exclude: /node_modules|vendor/,
                 use: [
                     {
                         loader: MiniCssExtractPlugin.loader,
@@ -160,7 +160,7 @@ module.exports = () => ({
             },
             {
                 test: /\.(gif|png|jpe?g|svg|webp)$/i,
-                exclude: /node_modules|bower_components|vendor/,
+                exclude: /node_modules|vendor/,
                 use: [
                     {
                         loader: 'file-loader',
@@ -172,7 +172,7 @@ module.exports = () => ({
             },
             {
                 test: /\.(woff|woff2)/,
-                exclude: /node_modules|bower_components|vendor/,
+                exclude: /node_modules|vendor/,
                 use: [
                     {
                         loader: 'file-loader',
