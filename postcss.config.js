539d048a97cfa606089775710bb448a2c9ac47cf
zdenekd <ditezdenek@gmail.com>
2020-03-29 14:23:59 +0200

    Fix eslint and stylelint for devel

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = () => ({
     modules: true,
     map: process.env.NODE_ENV === 'development',
     plugins: {
-        stylelint: {},
+        stylelint: process.env.NODE_ENV === 'development' ? {} : undefined,
         'postcss-import': {},
         'postcss-nested': {},
         'postcss-normalize': {},
@@ -11,9 +11,7 @@ module.exports = () => ({
             importFrom: 'src/assets/styles/utilities/media.css',
         },
         'postcss-custom-media': {},
-        'postcss-reporter': {
-            clearReportedMessages: true,
-        },
+        'postcss-reporter': {clearReportedMessages: true},
         cssnano: process.env.NODE_ENV === 'production',
     },
 });
