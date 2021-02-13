f5de0b7fea2a943a17134ea4fc8a3af22a4876da
zdenekd <ditezdenek@pm.me>
2021-02-13 17:36:41 +0100

    Add favicons

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -44,6 +44,13 @@ const Layout: React.FC<ILayout> = ({children, className = ''}) => {
                 <link rel="preconnect" href="https://fonts.gstatic.com/" crossOrigin="true" />
                 <link rel="dns-prefetch" href="https://fonts.googleapis.com" />
                 <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;600&display=swap&subset=latin-ext" rel="stylesheet" crossOrigin="anonymous" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
+                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+                <meta name="msapplication-TileColor" content="#6c6ce5" />
+                <meta name="theme-color" content="#ffffff" />
                 {process.env.NODE_ENV === 'production' && (
                     <script async defer data-domain="fit-s-marky.cz" src="https://plausible.io/js/plausible.js"></script>
                 )}
